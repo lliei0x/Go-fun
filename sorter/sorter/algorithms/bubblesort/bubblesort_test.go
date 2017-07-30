1268896474c0106571f0a0648147ecabd628c8aa
李磊 <lee@leeif.me>
2017-07-30 15:59:39 +0800

    modify:修改冒泡测试

--- sorter/sorter/algorithms/bubblesort/bubblesort_test.go
+++ sorter/sorter/algorithms/bubblesort/bubblesort_test.go
@@ -4,28 +4,28 @@ import "testing"
 import "fmt"
 
 func TestBubbleSort1(t *testing.T) {
-	valuse := []int{5, 4, 3, 2, 1}
-	BubbleSort(valuse)
+	values := []int{5, 4, 3, 2, 1}
+	BubbleSort(values)
 
-	if valuse[0] != 1 || valuse[1] != 2 || valuse[2] != 3 || valuse[3] != 4 || valuse[4] != 5 {
-		fmt.Println("BubbleSort() failed . Got ", valuse, "Expected 1 2 3 4 5")
+	if values[0] != 1 || values[1] != 2 || values[2] != 3 || values[3] != 4 || values[4] != 5 {
+		fmt.Println("BubbleSort() failed . Got ", values, "Expected 1 2 3 4 5")
 	}
 }
 
 func TestBubbleSort2(t *testing.T) {
-	valuse := []int{5, 5, 3, 2, 1}
-	BubbleSort(valuse)
+	values := []int{5, 5, 3, 2, 1}
+	BubbleSort(values)
 
-	if valuse[0] != 1 || valuse[1] != 2 || valuse[2] != 3 || valuse[3] != 5 || valuse[4] != 5 {
-		fmt.Println("BubbleSort() failed . Got ", valuse, "Expected 1 2 3 5 5")
+	if values[0] != 1 || values[1] != 2 || values[2] != 3 || values[3] != 5 || values[4] != 5 {
+		fmt.Println("BubbleSort() failed . Got ", values, "Expected 1 2 3 5 5")
 	}
 }
 
 func TestBubbleSort3(t *testing.T) {
-	valuse := []int{5}
-	BubbleSort(valuse)
+	values := []int{5}
+	BubbleSort(values)
 
-	if valuse[0] != 5 {
-		fmt.Println("BubbleSort() failed . Got ", valuse, "Expected 5")
+	if values[0] != 5 {
+		fmt.Println("BubbleSort() failed . Got ", values, "Expected 5")
 	}
 }

749cbe562015b3e23786e2ce24a901f53b38d242
lliei0x <git@liei.cc>
2017-07-30 15:37:53 +0800

    add:添加主程序

--- sorter/sorter/sorter.go
+++ sorter/sorter/sorter.go
@@ -2,5 +2,61 @@ package main
 
 import "flag"
 import "fmt"
+import "bufio"
+import "os"
+import "io"
+import "strconv"
 
-var
+var infile *string = flag.String("i", "infile", "File contains values for sorting")
+var outfile *string = flag.String("o", "outfile", "File to receive sorted values")
+var algorithm *string = flag.String("a", "qsort", "sort algorithm")
+
+func readValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("Failed to open the input file", infile)
+		return
+	}
+
+	defer file.Close()
+
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+		if isPrefix {
+			fmt.Println("A too long line , seems unexpected")
+			return
+		}
+		str := string(line)
+
+		values, err1 := strconv.Atoi(str)
+
+		if err1 != nil {
+			err = err1
+			return
+		}
+		values = append(values, value)
+	}
+
+	return
+}
+
+func main() {
+	flag.Parse()
+	if infile != nil {
+		fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+	}
+	values, err := readValues(*infile)
+	if err == nil {
+		fmt.Println("Read values:", values)
+	} else {
+		fmt.Println(err)
+	}
+}

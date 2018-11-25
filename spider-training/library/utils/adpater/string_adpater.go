bf657951d12147cc3d14c5e4b95d96a044320660
李磊 <1369183590@qq.com>
2018-11-26 00:57:42 +0800

    [update]

--- spider-training/library/utils/adpater/string_adpater.go
+++ spider-training/library/utils/adpater/string_adpater.go
@@ -2,9 +2,16 @@ package adapter
 
 import "strings"
 
-// StringClear 格式化字符串 去掉换行
+// StringClear 格式化字符串 去掉换行&空格
 func StringClear(str string) string {
-	newStr := strings.TrimSpace(str)
-	newReplacer := strings.NewReplacer("\n", "")
-	return newReplacer.Replace(newStr)
+	// newStr := strings.TrimSpace(str)
+	// newReplacer := strings.NewReplacer("\n", "")
+	// return newReplacer.Replace(newStr)
+
+	// 去除空格
+	str = strings.Replace(str, " ", "", -1)
+	// 去除换行符
+	str = strings.Replace(str, "\n", "", -1)
+
+	return str
 }

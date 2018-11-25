28dfff48518b0a640ef73bff1bed7ddd1a708620
lliei0x <git@liei.cc>
2018-11-25 22:35:27 +0800

    正则匹配抓取世界电影排行榜

--- spider-training/infra/downloader/downloader.go
+++ spider-training/infra/downloader/downloader.go
@@ -1,1 +1,37 @@
 package downloader
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+
+	"leeif.me/Go-fun/spider-training/infra/errors"
+)
+
+// GetHttpResponse get response for http request
+func GetHttpResponse(url string, ok bool) ([]byte, error) {
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, errors.ErrorRequest
+	}
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+	client := http.DefaultClient
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, errors.ErrorResponse
+	}
+	defer response.Body.Close()
+	fmt.Println(response.StatusCode)
+	if response.StatusCode <= 200 && response.StatusCode >= 300 {
+		return nil, errors.ErrorStatusCode
+	}
+	if ok {
+		utf8ContentReader := transform.NewReader(response.Body, simplifiedchinese.GBK.NewDecoder())
+		return ioutil.ReadAll(utf8ContentReader)
+	} else {
+		return ioutil.ReadAll(response.Body)
+	}
+}
--- spider-training/main.go
+++ spider-training/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"leeif.me/Go-fun/spider-training/domain/piaofang"
+	"leeif.me/Go-fun/spider-training/infra/url"
+)
+
+func main() {
+	piaofang.GetPiaofangRankInfo(url.PFangURL)
+}

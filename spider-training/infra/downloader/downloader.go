102627ed0a75d0c22539dcb5e5970ec469629ca6
lliei0x <git@liei.cc>
2018-11-28 22:13:58 +0800

    [update]处理post请求爬取中影数据

--- spider-training/infra/downloader/downloader.go
+++ spider-training/infra/downloader/downloader.go
@@ -1,8 +1,10 @@
 package downloader
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
@@ -32,4 +34,32 @@ func GetHttpResponse(url string, ok bool) ([]byte, error) {
 	} else {
 		return ioutil.ReadAll(response.Body)
 	}
+}
+
+// PostHttpResponse ...
+func PostHttpResponse(url string, body string, ok bool) ([]byte, error) {
+
+	payload := strings.NewReader(body)
+	requests, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		return nil, errors.ErrorRequest
+	}
+	requests.Header.Add("X-Requested-With", "XMLHttpRequest")
+	requests.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	requests.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+	client := http.DefaultClient
+	response, err := client.Do(requests)
+	if err != nil {
+		return nil, errors.ErrorResponse
+	}
+
+	fmt.Println(response.StatusCode)
+
+	defer response.Body.Close()
+	if ok {
+		utf8Content := transform.NewReader(response.Body, simplifiedchinese.GBK.NewDecoder())
+		return ioutil.ReadAll(utf8Content)
+	}
+	return ioutil.ReadAll(response.Body)
+
 }

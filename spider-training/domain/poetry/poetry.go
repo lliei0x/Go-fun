c329125a1eaf24c9664482265a19911f3aec579f
李磊 <1369183590@qq.com>
2018-12-02 18:24:59 +0800

    [updata] 增加朝代和类型

--- spider-training/domain/poetry/poetry.go
+++ spider-training/domain/poetry/poetry.go
@@ -122,3 +122,36 @@ func parsePoet(doc *goquery.Document) model.Poet {
 	})
 	return poet
 }
+
+//  朝代
+func parseDynasty() {
+	common(true)
+
+}
+
+// 诗的类型
+func parsePoetryType() {
+	common(false)
+}
+
+func common(ok bool) {
+	url := "https://www.gushiwen.org/shiwen/"
+	doc, _ := getDoc(url)
+	if ok {
+		doc.Find("div.right div.sons").Eq(2).Find("div.cont a").Each(func(i int, selection *goquery.Selection) {
+			fmt.Println(selection.Text())
+			var dynasty model.Dynasty
+			dynasty.Name = selection.Text()
+			initial.DataBase.Save(&dynasty)
+		})
+	} else {
+		doc.Find("div.right div.sons").Eq(3).Find("div.cont a").Each(func(i int, selection *goquery.Selection) {
+			fmt.Println(selection.Text())
+			var poetType model.PoetryType
+			poetType.TypeName = selection.Text()
+			initial.DataBase.Save(&poetType)
+		})
+
+	}
+
+}
